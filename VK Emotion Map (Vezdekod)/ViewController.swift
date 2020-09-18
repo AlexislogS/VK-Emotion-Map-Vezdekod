fba6045399968d8d5f558fba8c9cf8f5c4380de5
AlexislogS <55928329+AlexislogS@users.noreply.github.com>
2020-09-18 12:41:27 +0300

    Initial Commit

---
+++ VK Emotion Map (Vezdekod)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Emotion Map (Vezdekod)
+//
+//  Created by Alex Yatsenko on 18.09.2020.
+//  Copyright © 2020 AlexislogS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
