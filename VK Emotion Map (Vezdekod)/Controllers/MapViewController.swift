580e2e02428ac9caaa092f50dca433a48fb75295
AlexislogS <55928329+AlexislogS@users.noreply.github.com>
2020-09-18 15:34:32 +0300

    Handled keyboard show

--- VK Emotion Map (Vezdekod)/Controllers/MapViewController.swift
+++ VK Emotion Map (Vezdekod)/Controllers/MapViewController.swift
@@ -11,14 +11,63 @@ import MapKit
 
 final class MapViewController: UIViewController {
 
+    @IBOutlet weak var emotionView: UIView!
+    @IBOutlet private weak var emotionsViewHight: NSLayoutConstraint! {
+        didSet {
+            defaultEmotionsViewHight = emotionsViewHight.constant
+        }
+    }
     @IBOutlet private weak var emotionMap: MKMapView!
     @IBOutlet private weak var emotionCollectionView: UICollectionView!
     @IBOutlet private weak var emotionSearch: UISearchBar!
     
+    private var defaultEmotionsViewHight: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handleDidShow(notification:)),
+                                               name: UITextField.keyboardDidShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handleDidHide),
+                                               name: UITextField.keyboardDidHideNotification,
+                                               object: nil)
     }
     
+    @objc private func handleDidShow(notification: Notification) {
+        if let userInfo = notification.userInfo,
+            let keyboardFrame = (userInfo[UITextField.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            addAnimationToEmotionView(from: .fromTop) {
+                self.emotionsViewHight.constant = self.defaultEmotionsViewHight + keyboardFrame.height
+            }
+        }
+    }
+    
+    @objc private func handleDidHide() {
+        addAnimationToEmotionView(from: .fromBottom) {
+            self.emotionsViewHight.constant = self.defaultEmotionsViewHight
+        }
+    }
+    
+    private func addAnimationToEmotionView(from: CATransitionSubtype,
+                                           completion: () -> Void) {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = CATransitionType.moveIn
+        transition.subtype = from
+        self.emotionView.layer.add(transition, forKey: kCATransition)
+        completion()
+    }
+}
+
+    // MARK: - UISearchBarDelegate
+
+extension MapViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
 }
 
